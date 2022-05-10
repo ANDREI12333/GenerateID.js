d80dbe39a2d8ce2c83530182c783f8c86111dd16
Andrei Balan <abalanandreibalan8@yahoo.com>
2022-05-10 17:12:03 +0300

    Add files via upload

---
+++ GenerateID.js
@@ -0,0 +1,9 @@
+function GenerateID() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+    for (var i = 0; i < 11; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  
+    return text;
+}
